d607dea43801af970c14607838a78d7c14262909
Nithin Bekal <nithinbekal@gmail.com>
2016-03-20 03:13:10 +0530

    WIP

--- lib/game.rb
+++ lib/game.rb
@@ -2,13 +2,21 @@
 class Game
   attr_reader :cells
 
-  def initialize(input_as_text)
-    @cells = input_as_text.split("\n")
-      .map { |row| row.split(//) }
+  def initialize(text)
+    @cells = text.split("\n")
+      .map.with_index { |row, x| cell_tuple(row, x)}
+      .flatten
   end
 
   def next
     Board.new(@cells).next
+  end
+
+  def cell_tuple(line_text, x_index)
+    line_text.split(//)
+      .map.with_index { |value, y| [value, y] }
+      .select { |element| element[0] == '1' }
+      .map { |element| Cell.new(x_index, element[1]) }
   end
 end
 
@@ -16,33 +24,53 @@ class Board
   attr_reader :cells
 
   def initialize(cells)
-    @cells = cells.map.with_index { |row, x|
-      row.map.with_index { |value, y| Cell.new(x, y, value) }
-    }.flatten
+    @cells = cells
   end
 
   def next
+    # Board.new(surviving_cells + resurrecting_cells)
+    resurrecting_cells
+    Board.new([])
   end
 
-  def neighbors_of_all_live_cells
-    @cells.map(&:alive?)
+  private
+
+  def surviving_cells
+    candidates_for_next_turn
+      .select { |cell| alive?(cell) && has_neighbor_count?(cell, [2,3]) }
+  end
+
+  def resurrecting_cells
+    candidates_for_next_turn
+      .select { |cell| dead?(cell) && has_neighbor_count?(cell, [3]) }
+  end
+
+  def candidates_for_next_turn
+    @cells.map(&:neighborhood).uniq
+  end
+
+  def dead?(cell)
+    !alive?(cell)
+  end
+
+  def alive?(cell)
+    @cells.any? { |c| c == cell }
   end
 end
 
-class Cell < Struct.new(:x, :y, :value)
+class Cell < Struct.new(:x, :y)
   def ==(other)
-    value == other.value &&
-      x == other.x &&
-      y == other.y
+    self.x == other.x && self.y == other.y
   end
 
-  def alive?
-    value == '1'
-  end
-
-  def neighbor_coords
+  def neighborhood
     [x-1, x, x+1].product([y-1, y, y+1])
-      .reject { |element| element[0] == x && element[1] == y }
       .map { |element| Cell.new(element[0], element[1]) }
   end
+
+  def neighbors_in_board(board)
+    surrounding_cells = neighbors - self
+    []
+  end
 end
+
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,26 +5,21 @@ describe Game do
   it 'reads the input' do
     input = "01\n10"
     cells = Game.new(input).cells
-    cells.must_equal [
-      %w(0 1),
-      %w(1 0)
-    ]
+    cells.must_equal [ Cell.new(0, 1), Cell.new(1, 0) ]
   end
 
-  # Board
-
-  it 'loads cells correctly' do
-    input = "01"
-    cells = Board.new([[0,1]]).cells
-    cells.must_equal [Cell.new(0, 0, 0), Cell.new(0, 1, 1)]
+  it 'generates the next state' do
+    game = Game.new("0000\n0100\n0110\n0000")
+    expected = Game.new("0000\n0110\n0110\n0000")
+    expected.cells.must_equal(game.next.cells)
   end
+end
 
-  # Cell
-
+describe Cell do
   it 'calculates cell neighbor co-ordinates' do
-    cell = Cell.new(2, 6, 0)
-    coords = [[1, 5], [1, 6], [1, 7], [2, 5], [2, 7], [3, 5], [3, 6], [3, 7]]
-    cell.neighbor_coords.must_equal \
+    cell = Cell.new(2, 6)
+    coords = [[1, 5], [1, 6], [1, 7], [2, 5], [2, 6], [2, 7], [3, 5], [3, 6], [3, 7]]
+    cell.neighborhood.must_equal \
       coords.map { |c| Cell.new(c[0], c[1]) }
   end
 end
